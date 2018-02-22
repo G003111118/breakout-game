1ecb3ed6dc81f9685eed99539eb3123fd4415b4e
G003111118 <35809339+G003111118@users.noreply.github.com>
2018-02-22 16:27:51 +0000

    Bounce off wall

--- game/js/breakout.js
+++ game/js/breakout.js
@@ -1,6 +1,10 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+//radius of ball
+var ballRadius=10;
+
+
 //set the starting point
 var x=canvas.width/2;
 var y=canvas.height-30;
@@ -11,13 +15,22 @@ var dy=-2;
 //draw the ball
 function drawball(){
 	ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle= "#0095DD";
 	ctx.fill();
 	ctx.closePath();
 }
 
 function draw() {
+	//Bounce
+	if(y+dy>canvas.height-ballRadius||y+dy<ballRadius){
+	dy=-dy;
+}
+
+if(x+dx>canvas.width-ballRadius||x+dx<ballRadius){
+	dx=-dx;
+}
+
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawball();
 	
@@ -26,3 +39,6 @@ function draw() {
 }
 
 setInterval(draw, 10);
+
+
+
