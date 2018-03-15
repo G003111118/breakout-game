c83b9fba9db48d964e5e0f0dde155e8c2504e625
G003111118 <35809339+G003111118@users.noreply.github.com>
2018-03-15 16:54:21 +0000

    draw paddle

--- game/js/breakout.js
+++ game/js/breakout.js
@@ -14,8 +14,18 @@ var dy=-2;
 
 var ballColour = "#0095DD"
 
+//define paddle to hit the ball
+
+var paddleHeight=10;
+var paddleWidth=75;
+var paddleX=(canvas.width-paddleWidth)/2;
+
+//left or right variables
+var rightPressed=false;
+var leftPressed=false;
+
 //draw the ball
-function drawball(){
+function drawBall(){
 	ctx.beginPath();
 	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle= ballColour;
@@ -23,28 +33,66 @@ function drawball(){
 	ctx.closePath();
 }
 
+//draw paddle function 
+function drawPaddle(){
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+	ctx.fillStyle= "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
 function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
+	drawPaddle();
+	
+	if(rightPressed && paddleX <canvas.width-paddleWidth){
+		paddleX +=7;
+	}
+	else if(leftPressed && paddleX >0) {
+		paddleX -=7;
+	}
+	
 	//Bounce
 	if(y+dy>canvas.height-ballRadius||y+dy<ballRadius){
-	dy=-dy;
-	ballColour="blue";
-	ballRadius=10;
-}
+		dy=-dy;
+		ballColour="blue";
+		ballRadius=10;
+	}
 
-if(x+dx>canvas.width-ballRadius||x+dx<ballRadius){
-	dx=-dx;
-	ballColour = "red";
-	ballRadius=10;
-}
+	if(x+dx>canvas.width-ballRadius||x+dx<ballRadius){
+		dx=-dx;
+		ballColour = "red";
+		ballRadius=10;
+	}
 
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawball();
+
 	
 	x+=dx;
 	y+=dy;
+} //end of draw function
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = true;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = true;
+	}
+}
+
+
+function keyUpHandler(e) {
+	if(e.keyCode == 39){
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37){
+		leftPressed = false;
+	}
 }
 
 setInterval(draw, 10);
-
-
-
